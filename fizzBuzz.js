72c3808ce1bcb126c26030893c745f1991232c5c
Jorge Rodriguez <58515014+Jorge10504@users.noreply.github.com>
2023-09-18 21:03:13 -0500

    Update fizzBuzz.js
    Adding better indentation to the code to better understand where each function is coming from

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -8,15 +8,15 @@ function fizzBuzz(){
         if (count % 3 === 0 && count % 5 === 0){
             output.push("FizzBuzz");
         }
-        else if (count % 3 === 0){
-            output.push("Fizz");
-        }  
-        else if (count % 5 === 0){
-            output.push("Buzz");
-        }
-        else {
-            output.push(count);
-        }  
+            else if (count % 3 === 0){
+                output.push("Fizz");
+            }  
+            else if (count % 5 === 0){
+                output.push("Buzz");
+            }
+            else {
+                output.push(count);
+            }  
         count++;
     }
     console.log(output);

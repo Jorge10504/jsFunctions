5a6df1fe90bf381065239c3088c396efc6df88ae
Jorge Rodriguez <58515014+Jorge10504@users.noreply.github.com>
2024-02-09 22:21:44 -0500

    Update tipCalculator.js

--- tipCalculator.js
+++ tipCalculator.js
@@ -2,6 +2,7 @@ const bills = [100, 10, 1000];
 const tips = [];
 const totalBill = [];
 
+// This function takes in a bill value and calculates the tip amount
 function calcTip(bill) {
     if (bill >= 50 && bill <= 300){
         const tip = bill * 0.15;
@@ -12,6 +13,7 @@ function calcTip(bill) {
     }
 }
 
+// This function adds the tip amounts to a new array for each bill value
 function tipArray(billArray) {
     for (let i = 0; i < billArray.length; i++) {
         const tip = calcTip(billArray[i]);
@@ -20,6 +22,7 @@ function tipArray(billArray) {
     return tips;
 }
 
+// Add the total bill to a new array by summing up the bill and tip values
 function addTip(tips, bills){
     for (let i = 0; i < tips.length; i++) {
         const total = tips[i] + bills[i];
